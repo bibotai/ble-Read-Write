cd3a45fefc8ff250e9d59e0d15b858816d129e5f
bibotai <bibotaixsf@gmail.com>
2016-12-16 11:18:48 +0800

    simple scan

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-var noble = require('noble');
+// var noble = require('noble');
+import {noble} from 'noble';
+state = "poweredOn"
+noble.on('stateChange', callback(state));
+
 noble.startScanning(); // any service UUID, no duplicates
