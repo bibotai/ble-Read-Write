f041e0001aa21c3d5102ae21d83da5a12de0ec86
bibotai <bibotaixsf@gmail.com>
2016-12-25 18:00:28 +0800

    test

--- ihealthdevicesdk/communication/base/ble/blebase.js
+++ ihealthdevicesdk/communication/base/ble/blebase.js
@@ -12,7 +12,10 @@ export const discoveryServices = ({macAddress}) => {
                             // console.log('connected to peripheral: ' + peripheral.uuid);
                             peripheral
                                 .discoverServices(['636f6d2e6a6975616e2e414d56313200'], function (error, services) {
-                                    return services;
+                                    setTimeout(function () {
+                                        return services;
+                                    }, 5000)
+
                                 })
                         })
                 })
