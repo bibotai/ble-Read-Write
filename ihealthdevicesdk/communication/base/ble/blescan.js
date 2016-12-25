378d2b1a06daaf0109982ab9ea145f1f76e449ca
bibotai <bibotaixsf@gmail.com>
2016-12-25 23:41:08 +0800

    test

--- ihealthdevicesdk/communication/base/ble/blescan.js
+++ ihealthdevicesdk/communication/base/ble/blescan.js
@@ -24,7 +24,7 @@ export const promise = new Promise(function (resolve, reject) {
         if (peripheral.id === '00:4d:32:07:90:4b' || peripheral.address === '00:4d:32:07:90:4b') {
             stopScan();
             console.log(`device 00:4d:32:07:90:4b is found.`);
-            resolve(peripheral);
+            return resolve(peripheral);
         } else {
             reject(new Error('not found'));
         }
