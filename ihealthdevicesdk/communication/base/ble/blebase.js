f96cfc20b1ae680264fd9aa409b94e66ee1a583d
bibotai <bibotaixsf@gmail.com>
2016-12-25 18:10:46 +0800

    test

--- ihealthdevicesdk/communication/base/ble/blebase.js
+++ ihealthdevicesdk/communication/base/ble/blebase.js
@@ -3,22 +3,22 @@ var noble = require('noble');
 export const sendData = ({macAddress, command}) => {};
 
 export const discoveryServices = ({macAddress}) => {
-    noble
-        .on('discover', function (peripheral) {
-            peripheral
-                .connect(function (error) {
-                    console.log('connected to peripheral: ' + peripheral.uuid);
-                    peripheral.connect(function (error) {
-                        // console.log('connected to peripheral: ' + peripheral.uuid);
-                        peripheral
-                            .discoverServices([], function (error, services) {
-                                console.log(services.length);
-                                services.map((service, index) => {
-                                    console.log(service.uuid);
-                                })
-
+    console.log(macAddress);
+    noble.on('discover', function (peripheral) {
+        peripheral
+            .connect(function (error) {
+                console.log('connected to peripheral: ' + peripheral.uuid);
+                peripheral.connect(function (error) {
+                    // console.log('connected to peripheral: ' + peripheral.uuid);
+                    peripheral
+                        .discoverServices([], function (error, services) {
+                            console.log(services.length);
+                            services.map((service, index) => {
+                                console.log(service.uuid);
                             })
-                    })
+
+                        })
                 })
-        });
+            })
+    });
 };
