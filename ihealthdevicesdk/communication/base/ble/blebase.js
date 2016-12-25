b398ac152eff71ea83c963f6cad778a1c186fdc2
bibotai <bibotaixsf@gmail.com>
2016-12-26 00:21:19 +0800

    test

--- ihealthdevicesdk/communication/base/ble/blebase.js
+++ ihealthdevicesdk/communication/base/ble/blebase.js
@@ -1,33 +1,56 @@
 var noble = require('noble');
-import {scanDevice, promise} from './blescan';
+import {scanDevice} from './blescan';
 
 export const sendData = (macAddress, command) => {};
 
-export const discoveryServices = (macAddress) => {
-    // 先扫描是否存在这个设备 console.log(scanDevice(macAddress));
-    scanDevice(macAddress)
-        .then((peripheral) => {
-            console.log('then')
-            if (peripheral) {
-                console.log(peripheral);
-                //连接设备（相当于gatttool的connect）
-                peripheral.connect(function (error) {
-                    if (error) {
-                        console.log('an error occurred in peripheral.connect to: ' + peripheral.uuid);
-                    }
-                    console.log('connected to device: ' + peripheral.uuid);
-                    peripheral.discoverServices(['636f6d2e6a6975616e2e414d56313200'], function (error, services) {
-                        services.map((service, index) => {
-                            console.log(service.uuid);
+export const discoveryServices = (macAddress, serviceUuidArr) => {
+    const promise = new Promise(function (resolve, reject) {
+        // 先扫描是否存在这个设备 console.log(scanDevice(macAddress));
+        scanDevice(macAddress)
+            .then((peripheral) => {
+                console.log('then')
+                if (peripheral) {
+                    console.log(peripheral);
+                    //连接设备（相当于gatttool的connect）
+                    peripheral.connect(function (error) {
+                        if (error) {
+                            console.log('an error occurred in peripheral.connect to: ' + peripheral.uuid);
+                        }
+                        console.log('connected to device: ' + peripheral.uuid);
+                        //查找服务
+                        peripheral.discoverServices(serviceUuidArr, function (error, services) {
+                            if (error) {
+                                reject(error);
+                            } else {
+                                console.log(`services ${serviceUuidArr.join(',')} are found.`);
+                                resolve(services);
+                            }
+
                         });
-
                     });
-                });
-            } else {
-                console.log(`device ${macAddress} is not found.`);
-            }
-        }, function (error) {
-            console.log('e');
-        });
-
+                } else {
+                    console.log(`device ${macAddress} is not found.`);
+                }
+            }, function (error) {
+                console.log('e');
+            });
+    })
+    return promise;
 };
+
+export const discoveryCharacteristics = (service, characteristicsUuidArr) => {
+    const promise = new Promise(function (resolve, reject) {
+        //查找特性
+        service
+            .discoverCharacteristics(characteristicsUuidArr, function (error, characteristics) {
+                if (error) {
+                    reject(error);
+                } else {
+                    console.log(`characteristic ${characteristicsUuidArr.join(',')} are found.`);
+                    resolve(characteristics);
+                }
+
+            });
+    });
+    return promise;
+}
--- test/am4test.js
+++ test/am4test.js
@@ -1,6 +1,8 @@
-import {discoveryServices} from '../ihealthdevicesdk/communication/base/ble/blebase';
+import {discoveryServices, discoveryCharacteristics} from '../ihealthdevicesdk/communication/base/ble/blebase';
 const peripheralIdOrAddress = process
     .argv[2]
     .toLowerCase();
 console.log(peripheralIdOrAddress);
-discoveryServices(peripheralIdOrAddress);
+discoveryServices(peripheralIdOrAddress, ['636f6d2e6a6975616e2e414d56313200']).then((service) => {
+    discoveryCharacteristics(service, ['7365642e6a6975616e2e414d56313200']);
+});
